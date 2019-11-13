db7ee08b02d4e51eec24e7cd439339da779cdd51
Vladimir Kutuev <VladimirKutuev@gmail.com>
2019-11-13 20:11:10 +0300

    Added ThreadPool class

---
+++ ThreadPool/ThreadPool/ThreadPool.cs
@@ -0,0 +1,16 @@
+﻿namespace ThreadPool
+{
+  public class ThreadPool
+  {
+    private const int DefaultPoolSize = 4;
+
+    public ThreadPool(int poolSize)
+    {
+    }
+    
+    public ThreadPool() : this(DefaultPoolSize)
+    {
+    }
+
+  }
+}
