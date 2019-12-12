2740afe718d5202bfb54dfe055f707ba9c5c43a3
CherepanovAleksei <mrneumann888@gmail.com>
2019-12-12 23:22:50 +0300

    interface, realization and app, using appDomains to run untrusted code

---
+++ CalcLibInterface/CalcLibInterface/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.IO;
+namespace CalculatorInterface
+{
+    public interface ICalculator
+    {
+        int Sum(int a, int b);
+    }
+}
---
+++ CalcRealizationLib/CalcRealizationLib/Class1.cs
@@ -0,0 +1,19 @@
+﻿using CalculatorInterface;
+using System;
+using System.IO;
+
+namespace CalcRealizationLib
+{
+    [Serializable]
+    public class CalcRealization : ICalculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a+b;
+        }
+        public void ReadFileSystem()
+        {
+            File.ReadAllText("C:\\Temp\\file.txt");
+        }
+    }
+}
